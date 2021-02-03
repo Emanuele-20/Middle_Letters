625202271783557fcb26d9ba83d487e7b0c38fce
Emanuele Pace <emanuele.10@outlook.it>
2021-02-03 21:02:24 +0000

    Added another test for word with even length

--- middleLetter.test.js
+++ middleLetter.test.js
@@ -1,13 +1,18 @@
+const { expect } = require('@jest/globals')
 const middleLetter = require('./middleLetter')
 
 describe('Middle Letter', () => {
-    it('If the words length is even, return the 2 middle letter', () => {
+    it('Return the 2 middle letter if the words length is even', () => {
         expect(middleLetter('test')).toBe('es')
     })
-    it('If the word length is odd, return the middle letter', () => {
+    it('Return the middle letter if the word length is odd', () => {
         expect(middleLetter('testing')).toBe('t')
     })
-    it('If the word length is even, return the 2 middle letter', () => {
+    it('Return the 2 middle letter if the word length is even, longer word', () => {
         expect(middleLetter('middle')).toBe('dd')
+    })
+    it('Return one letter when the input is a single letter', () => {
+        expect(middleLetter('A')).toBe("A")
+
     })
 })
